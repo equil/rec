31297018739e2b9199aac77bfa90534f989da2c7
fredformout <fredformout@yandex.ru>
2012-09-02 09:58:37 +0400

    db fix

--- receipt/dataAccess/model/Ingredient.h
+++ receipt/dataAccess/model/Ingredient.h
@@ -2,18 +2,19 @@
 //  Ingredient.h
 //  receipt
 //
-//  Created by Alexey Rogatkin on 16.08.12.
-//
+//  Created by fredformout on 02.09.12.
+//  Copyright (c) 2012 Alexey Rogatkin. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
-@class Product;
+@class Product, Receipt;
 
 @interface Ingredient : NSManagedObject
 
 @property (nonatomic, retain) NSDecimalNumber * weight;
 @property (nonatomic, retain) Product *product;
+@property (nonatomic, retain) Receipt *receipt;
 
 @end
--- receipt/dataAccess/model/Ingredient.m
+++ receipt/dataAccess/model/Ingredient.m
@@ -2,17 +2,19 @@
 //  Ingredient.m
 //  receipt
 //
-//  Created by Alexey Rogatkin on 16.08.12.
-//
+//  Created by fredformout on 02.09.12.
+//  Copyright (c) 2012 Alexey Rogatkin. All rights reserved.
 //
 
 #import "Ingredient.h"
 #import "Product.h"
+#import "Receipt.h"
 
 
 @implementation Ingredient
 
 @dynamic weight;
 @dynamic product;
+@dynamic receipt;
 
 @end
